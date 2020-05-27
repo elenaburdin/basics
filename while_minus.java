3215662eb6bb18dabb9244041a81865160f12052
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2020-05-27 13:43:57 +0300

    Add files via upload

---
+++ while_minus.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class while_minus {
+
+	public static void main(String[] args) {
+		
+		int i = 10;
+		
+		while ( i >= 1 ) {
+			System.out.println( i );
+			
+			i--;
+		}
+		
+		
+		
+
+	}
+
+}
