b21b596e2b249e672ee7902f35cfc88366d6a98a
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2020-05-31 13:40:21 +0300

    Add files via upload

---
+++ app_For.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class app_For {
+
+	public static void main(String[] args) {
+		
+
+		for( int n = 1; n < 11; n++ ) {
+			if( n == 3) {
+				continue;
+			}
+			   System.out.println( n );
+			}
+	 }
+
+}
