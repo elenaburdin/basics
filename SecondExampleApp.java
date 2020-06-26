87af0dc854bb504f88223c4bd923d8b7f9667f9a
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2020-06-26 21:08:39 +0300

    Add files via upload

---
+++ SecondExampleApp.java
@@ -0,0 +1,72 @@
+package basics;
+
+import java.lang.Math;
+
+public class SecondExampleApp {
+	
+	public static void main(String[] args) {
+		
+		 Circle bigCircle = new Circle( 11, 0, 100 );
+		 Circle smallCircle = new Circle( 0, -10, 10 );
+
+		 
+		        bigCircle.print();
+		        smallCircle.print();
+		
+	}
+
+}
+
+class Circle {
+	 
+	int cx;
+	int cy;
+	int radius;
+	boolean valid;
+	
+	
+	//constructor
+	Circle(int pcx, int pcy, int pr) {
+	//	System.out.println("It works");
+		cx = pcx;
+		cy= pcy;
+		if( pr >= 0 ) {
+			radius = pr;
+			valid = true;
+		} else {
+			System.out.println("NEGATIVE RADIUS VALUE!!!");
+			valid = false;
+		}
+		
+		
+	}
+	
+	//printing logic
+	void print() {
+		if( valid ) {
+		    System.out.println(
+				"########## CIRCLE ##########\n" +
+		        "RADIUS: " + radius + "\n"       +
+		        "center: " + cx + "," + cy + "\n"+ 
+		        "############################\n" 
+				);
+		} else {
+			System.out.println(
+			    "########## CIRCLE (BROKEN!!!)##########\n"
+				);
+		}
+		
+	}
+	//it will check: if valid   -> will return area
+	//               if invalid -> will return -1
+	float calculateArea() {
+		if ( valid ) {
+		float area = ( float ) ( radius * radius * Math.PI );
+			  return ( float ) area;
+	} else {
+		      return -1;
+    }
+  }
+
+}
+  
